3ae881821975c0a43dda0052d66ae7f56a8e2fff
spiosra <spiosra@spiosrafox.net>
2020-06-07 13:15:19 -0400

    Add more elegant dynamic configuration solutions to Polybar

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,4 +1,18 @@
 #!/bin/sh
+# Kill existing instances
 pkill polybar
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-polybar bar &
+while pgrep -u "$UID" -x polybar > /dev/null; do
+    sleep 1
+done
+
+# No thermal zones and hwmon files change? Working around it...
+name="amdgpu"
+for hwmon in /sys/class/hwmon/*; do
+    grep "$name" "$hwmon/name" > /dev/null
+    if [ "$?" -eq 0 ]; then
+        hwmon="$hwmon/temp1_input"
+        break
+    fi
+done
+
+NETWORK_SCRIPT="$XDG_CONFIG_HOME/polybar/scripts/network.sh" HWMON="$hwmon" polybar bar &
--- bin/get-temp-hwmon.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-name="amdgpu"
-for hwmon in /sys/class/hwmon/*; do
-    grep "$name" "$hwmon/name" 1>/dev/null
-    if [ "$?" -eq 0 ]; then
-        echo "$hwmon/temp1_input"
-        break
-    fi
-done
