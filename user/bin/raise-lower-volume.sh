1b2bbda95d9405604bd2a114858f50f69051525e
spiosra <spiosra@spiosrafox.net>
2020-10-01 01:05:35 -0400

    Several minor changes related to Polybar's audio and microphone modules. Add sound effects, public domain.

---
+++ user/bin/raise-lower-volume.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+if [ "$1" = "raise" ] || [ "$1" = "lower" ]; then
+    audio="$XDG_DATA_HOME/media/audio/beep.wav"
+    current="$(pacmd dump-volumes | awk 'NR==1 {print $8}' | sed 's/\%//')"
+    if [ "$1" = "raise" ]; then
+        if [ "$current" -lt 100 ]; then
+            [ "$current" -lt 99 ] && pactl set-sink-volume @DEFAULT_SINK@ +2% || pactl set-sink-volume @DEFAULT_SINK@ +1%
+            exec paplay "$audio"
+        fi
+    else
+        if [ "$current" -gt 0 ]; then
+            pactl set-sink-volume @DEFAULT_SINK@ -2%
+            exec paplay "$audio"
+        fi
+    fi
+fi
--- user/bin/raise-vol.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-current="$(pacmd dump-volumes | awk 'NR==1 {print $8}' | sed 's/\%//')"
-if [ "$current" -lt 100 ]; then
-    [ "$current" -lt 99 ] && pactl set-sink-volume @DEFAULT_SINK@ +2% || pactl set-sink-volume @DEFAULT_SINK@ +1%
-fi
