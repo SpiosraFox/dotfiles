edeb13fdfd8ee628d863d122f6917bff44d962ed
Spíosra <spiosra@spiosrafox.net>
2021-01-18 08:15:48 +0000

    Update pacman kernel hook and script

--- sys/usr/local/sbin/sign-kernel.sh
+++ sys/usr/local/sbin/sign-kernel.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 . /usr/local/lib/sign-kernel-env.sh
-sbverify --cert "$PUBLIC_KEY" /boot/vmlinuz-linux >/dev/null 2>&1
-if [ "$?" -ne 0 ]; then
-    sudo -u "$SUSER" sh -c "gpg -o- -d $PRIVATE_KEY 2>/dev/null" | sbsign --output /boot/vmlinuz-linux --key /dev/stdin --cert "$PUBLIC_KEY" /boot/vmlinuz-linux 2>/dev/null
-fi
+while read -r kernel; do
+    sbverify --cert "$PUBLIC_KEY" /boot/vmlinuz-"$kernel" >/dev/null 2>&1
+    if [ "$?" -ne 0 ]; then
+        sudo -u "$SUSER" sh -c "gpg -o- -d $PRIVATE_KEY 2>/dev/null" | sbsign --output /boot/vmlinuz-"$kernel" --key /dev/stdin --cert "$PUBLIC_KEY" /boot/vmlinuz-"$kernel" 2>/dev/null
+    fi
+done
