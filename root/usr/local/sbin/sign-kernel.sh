3f59ed85ad9af350c43e4590c14f2579580c1527
spiosra <spiosra@spiosrafox.net>
2021-05-02 06:18:06 +0000

    Edit muttrc and sign-kernel.sh

--- root/usr/local/sbin/sign-kernel.sh
+++ root/usr/local/sbin/sign-kernel.sh
@@ -2,11 +2,11 @@
 # Sign kernel files for secure boot.
 #
 # Parameters.
-#   $1: Age-encrypted secret key.
+#   $1: Secret key.
 #   $2: Public key.
 
 while read -r kernel; do
     if ! sbverify --cert "${2}" /boot/vmlinuz-"${kernel}" > /dev/null 2>&1; then
-        age -d "${1}" | sbsign --output /boot/vmlinuz-"${kernel}" --key /dev/stdin --cert "${2}" /boot/vmlinuz-"${kernel}" 2> /dev/null
+        sbsign --output /boot/vmlinuz-"${kernel}" --key "${1}" --cert "${2}" /boot/vmlinuz-"${kernel}" 2> /dev/null
     fi
 done
